669241926bd0ae1bd7e1960125ec43a113963ae5
leehyunmin <leehm2292@gmail.com>
2026-01-08 13:33:14 +0900

    260108_2

--- src/Test/CollectionEx1.java
+++ src/Test/CollectionEx1.java
@@ -1,27 +1,113 @@
 package Test;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CollectionEx1 {
     public static void main(String[] args) {
-        ArrayList<Person> p = new ArrayList<Person>();
+        ArrayList<Student> p = new ArrayList<Student>();
+        Scanner scan = new Scanner(System.in);
 
-        p.add(new Person("이현민", "서울 관악구", "010-3496-2292"));
-        p.add(new Person("이현민", "강원도 양구", "010-3496-2292"));
-        p.add(new Person("이현민", "미국 샌프란시스코", "010-3496-2292"));
+        while(true) {
+            String name, stuld, age, phone;
+
+            System.out.print("이름 >>");
+            name = scan.next();
+            if(name.equals("exit")) break;
+            System.out.print("학번 >>");
+            stuld = scan.next();
+            if(stuld.equals("exit")) break;
+            System.out.print("나이 >>");
+            age = scan.next();
+            if(age.equals("exit")) break;
+            System.out.print("전화번호 >>");
+            phone = scan.next();
+            if(phone.equals("exit")) break;
+
+            p.add(new Student(name, stuld, age, phone));
+        }
+
 
         for(int i=0; i<p.size(); i++) System.out.println(p.get(i));
-        for(Person member : p) System.out.println(member);
+        for(Student member : p) System.out.println(member);
+
 
         ArrayList<Customer> c = new ArrayList<>();
 
-        c.add(new Customer("이현민", "서울 관악구", "010-3496-2292",1,10000));
-        c.add(new Customer("이현민", "서울 관악구", "010-3496-2292",2,100));
-        c.add(new Customer("이현민", "서울 관악구", "010-3496-2292",3,5000));
+        while(true) {
+            String name, addr, phone;
+            int cusNo, mile;
 
-        for(int i=0; i<c.size(); i++) if(c.get(i).getMile() >= 1000) System.out.println(c.get(i));
-        for(Customer member : c) if(member.getMile() >= 1000) System.out.println(member);
+            System.out.print("이름 >>");
+            name = scan.next();
+            if(name.equals("exit")) break;
+            System.out.print("주소 >>");
+            addr = scan.next();
+            if(addr.equals("exit")) break;
+            System.out.print("전화번호 >>");
+            phone = scan.next();
+            if(phone.equals("exit")) break;
+            System.out.print("학생번호를 입력하세요 >>");
+            cusNo = scan.nextInt();
+            System.out.print("마일 수를 입력하세요 >>");
+            mile = scan.nextInt();
+
+            c.add(new Customer(name, addr, phone, cusNo, mile));
+        }
+
+        for (int i=0; i<c.size(); i++) if(c.get(i).getMile() >= 1000) System.out.println(c.get(i));
+        for (Customer member : c) if(member.getMile() >= 1000) System.out.println(member);
 
 
+        ArrayList<Magazine> magazines = new ArrayList<>();
+
+        while(true) {
+            String title, author;
+            int pages, date;
+
+            System.out.print("책 제목 >>");
+            title = scan.next();
+            if(title.equals("exit")) break;
+            System.out.print("책 작가 >>");
+            author = scan.next();
+            if(author.equals("exit")) break;
+            System.out.print("책 페이지 수 >>");
+            pages = scan.nextInt();
+            System.out.println("책 출간 날짜 >>");
+            date = scan.nextInt();
+
+            magazines.add(new Magazine(title, author, pages, date));
+        }
+
+        for (int i=0; i<magazines.size(); i++) System.out.println(magazines.get(i));
+        for (Magazine books : magazines) System.out.println(books);
+
+
+        ArrayList<TextBook> textBooks = new ArrayList<>();
+
+        while(true) {
+            String title, author, school, hakgi;
+            int pages;
+
+            System.out.print("책 제목 >>");
+            title = scan.next();
+            if(title.equals("exit")) break;
+            System.out.print("책 작가 >>");
+            author = scan.next();
+            if(author.equals("exit")) break;
+            System.out.print("책 페이지 수 >>");
+            pages = scan.nextInt();
+            System.out.print("학교 이름 >>");
+            school = scan.next();
+            if(school.equals("exit")) break;
+            System.out.print("학년-학기 >>");
+            hakgi = scan.next();
+            if(hakgi.equals("exit")) break;
+
+            textBooks.add(new TextBook(title, author, pages, school, hakgi));
+        }
+
+        for (int i=0; i<textBooks.size(); i++) if(textBooks.get(i).getSchool().equals("미림마이스터고")) System.out.println(textBooks.get(i));
+        for (TextBook tb : textBooks) if(tb.getSchool().equals("미림마이스터고")) System.out.println(tb);
     }
 }
